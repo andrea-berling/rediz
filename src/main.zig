b183c32836316dd7c4f10333e6ffb2695fbc9a16
Andrea Berlingieri <andrea-berling@users.noreply.github.com>
2025-05-15 17:54:30 +0200

    Pass first stage

--- src/main.zig
+++ src/main.zig
@@ -8,18 +8,18 @@ pub fn main() !void {
     try stdout.print("Logs from your program will appear here!", .{});
 
     // Uncomment this block to pass the first stage
-    //
-    // const address = try net.Address.resolveIp("127.0.0.1", 6379);
-    //
-    // var listener = try address.listen(.{
-    //     .reuse_address = true,
-    // });
-    // defer listener.deinit();
-    //
-    // while (true) {
-    //     const connection = try listener.accept();
-    //
-    //     try stdout.print("accepted new connection", .{});
-    //     connection.stream.close();
-    // }
+
+    const address = try net.Address.resolveIp("127.0.0.1", 6379);
+
+    var listener = try address.listen(.{
+        .reuse_address = true,
+    });
+    defer listener.deinit();
+
+    while (true) {
+        const connection = try listener.accept();
+
+        try stdout.print("accepted new connection", .{});
+        connection.stream.close();
+    }
 }
